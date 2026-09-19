7c68d52f50a7fc7e2bdb00841d9e8d4f139d3bca
capcyr <capcyrusli@gmail.com>
2026-09-19 11:47:29 +0100

    Add files via upload

---
+++ Object Oriented Programming/practice.py
@@ -0,0 +1,12 @@
+
+class Cat():
+    species = "mammal"
+    sound = "Meow"
+
+    def speak(self):
+        print(f"I am a {self.species}, and I say {self.sound}" )
+
+
+kitty = Cat()
+
+kitty.speak()
